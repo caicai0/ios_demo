f3d7c75c874da691b218a5e484d35269cb1b4487
liyufeng <972008514@qq.com>
2017-03-02 16:18:12 +0800

    jspatch

---
+++ CAIReader_demo/CAIReader/CAIReader/book.swift
@@ -0,0 +1,15 @@
+//
+//  book.swift
+//  CAIReader
+//
+//  Created by liyufeng on 2017/2/28.
+//  Copyright © 2017年 liyufeng. All rights reserved.
+//
+
+import UIKit
+
+class book: NSObject {
+    init(path:NS) {
+        
+    }
+}
---
+++ JSPatch_demo/jspatch_demo/jspatch_demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  jspatch_demo
+//
+//  Created by liyufeng on 2017/3/2.
+//  Copyright © 2017年 liyufeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
